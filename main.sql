6adf67d649e10f5765e1026584f1deddf922284c
Ezequiel Dastolfo <ezequieldastolfo@gmail.com>
2023-01-26 20:20:17 -0500

    new data

--- main.sql
+++ main.sql
@@ -172,4 +172,249 @@ FROM tabla_diaria
 LIMIT 1500;
 
 
+-- SELECT es un tipo de consulta que se puede anidar en "SUB QUERY" (entre paréntesis)
+SELECT *
+FROM   (
+	SELECT ROW_NUMBER() OVER() AS row_id, *
+	FROM tabla
+) 
+WHERE row_id = 5
+;
+
+-- DISTINCT trae el dato solo una vez
+
+--CONCAT es la sentencia que nos permite concatenar dos valores diferentes indepentientemente de donde los vayamos a traer
+SELECT CONCAT(a.nombre, ' ', a.apellido)
+
+
+
+/* ALGUNOS COMANDOS */
+
+SELECT *
+FROM   tabla
+FETCH FIRST 1 ROWS ONLY;
+
+SELECT *
+FROM   tabla
+LIMIT 1;
+-- En ambos casos se obtiene el primer elemento de la lista
+
+
+SELECT *
+FROM (
+	SELECT ROW_NUMBER() OVER() AS row_id, *
+	FROM tabla
+) AS with_row_num
+WHERE row_id IN(1,5,10,20,50,71);
+-- Trae una lista específica de elementos
+
+SELECT *
+FROM tabla
+WHERE id IN (
+	SELECT id
+	FROM tabla
+	WHERE row_id = 30
+)
+--Con el sub query podemos crear una condición para que traiga una cantidad específica de elementos que cumplan con esa condición
+
+--DATE_PART es una forma de extraer la parte de una fecha, tal como el año, el mes o el día.
+SELECT DATE_PART('YEAR', fecha_) AS year_inc,
+DATE_PART('MONTH', fecha_) AS month_inc,
+DATE_PART('DAY', fecha_) AS day_inc
+FROM tabla;
+
+-- O incluso horas, miutos y segundos
+
+SELECT DATE_PART('HOUR', fecha_) AS hour_inc,
+DATE_PART('MINUTE', fecha_) AS minute_inc,
+DATE_PART('SECOND', fecha_) AS second_inc,
+FROM tabla;
+-- Al utilizar AS se crea una nueva columna en la tabla con la información extraida
+
+-- EXTRACT obtiene un dato específico, por ejemplo de una fecha
+SELECT *
+FROM tabla
+WHERE (EXTRACT(YEAR FROM fecha_)) = 2018
+-- Es una alternativa para DATE_PART
+
+--CAST en SQL determina la forma como deseamos que se trate ese tipo de dato por ejemplo:
+::text
+::timestamp
+
+SELECT (tabla.*)::text, COUNT(*)
+FROM tabla
+GROUP BY tabla.*
+-- En este ejemplo se extraen todos los elementos de cada columna y se retornan en una sola columna separados por comas
+
+
+-- Self JOIN. Unir elementos de la misma tabla
+SELECT a.nombre,
+		t.nombre,
+FROM tabla AS a
+	INNER JOIN tabla AS t ON a.columna_id = t.id
+
+
+  /* ALGUNAS FUNCIONES */
+
+--lpad() agrega una cantidad de padding a la izquierda
+SELECT rpad('*', id, '*'), carrera_id
+FROM tabla
+WHERE id < 10
+--rpad() agrega una cantidad de padding a la derecha
+
+--generate_series() genera automáticamente un determinado rango
+SELECT *
+FROM generate_series(1,10)
+
+--generate_series puede operar con un tercér parámetro, por ejemplo para ir en orden descendente
+SELECT *
+FROM generate_series(10,1,-2)
+-- el tercer parámetro es lla forma en la que ira descendiendo el generador
+
+
+--current_date es una función que devuelve la fecha actual
+--current_time devuelve hora y zona horaria
+
+/* EXPRESIONES REGULARES */
+
+SELECT email
+FROM platzi.alumnos
+WHERE email ~*'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}';
+
+SELECT email
+FROM platzi.alumnos
+WHERE email ~*'[A-Z0-9._%+-]+@google[A-Z0-9.-]+\.[A-Z]{2,4}';
+
+/* UN POCO OSBRE BASES DE DATOS */
+
+/*
+BASES DE DATOS DISTRIBUIDAS: 
+Son una colección de múltiples bases de datos separadas físicamente que se comunican mediante una red informática.
+
+VENTAJAS:
+
+-desarrollo modular.
+-incrementa la confiabilidad.
+-mejora el rendimiento.
+-mayor disponibilidad.
+-rapidez de respuesta.
+
+DESVENTAJAS:
+
+-Manejo de seguridad.
+-complejidad de procesamiento.
+-Integridad de datos más compleja.
+-Costo.
+
+TIPOS:
+
+Homogéneas: mismo tipo de BD, manejador y sistema operativo. (aunque esté distribuida).
+Heterogénea: puede que varíen alguna de los anteriores características.
+-OS
+-Sistema de bases de datos.
+-Modelo de datos.
+
+ARQUITECTURAS:
+
+-** cliente- servidor**: donde hay una BD principal y tiene varias BD que sirven como clientes o como esclavas que tratarán de obtener datos de la principal, a la que normalmente se hacen las escrituras.
+
+Par a par (peer 2 peer): donde todos los puntos en la red de bd son iguales y se hablan como iguales sin tener que responder a una sola entidad.
+multi manejador de bases de datos.
+ESTRATEGIA DE DISEÑO:
+
+top down: es cuando planeas muy bien la BD y la vas configurando de arriba hacia abajo de acuerdo a tus necesidades.
+bottom up: ya existe esa BD y tratas de construir encima.
+
+ALMACENAMIENTO DISTRIBUIDO:
+
+-Fragmentación: qué datos van en dónde.
+
+fragmentación horizontal: (sharding) partir la tabla que estás utilizando en diferentes pedazos horizontales.
+
+fragmentación vertical: cuando parto por columnas.
+
+fragmentación mixta: cuando tienes algunas columnas y algunos datos en un lugar y algunas columnas y algunas tuplas en otro lugar.
+
+-Replicación: tienes los mismos datos en todas ala BBDD no importa donde estén.
+
+-replicación completa: cuando toda al BD está en varias versiones a lo largo del globo, toda la información está igualita en todas las instancias de BD.
+-replicación parcial: cuando algunos datos están replicados y compartidos en varias zonas geográficas
+-sin replicación: no estás replicando nada de los datos, cada uno está completamente separa y no tienen que estarse hablando para sincronizar datos entre ellas.
+
+DISTRIBUCIÓN DE DATOS:
+
+-Distribución: cómo va a pasar la data entre una BD y otra. Tiene que ver mucho con networking, tiempos, latencia, etc. Pueden ser:
+
+Centralizada: cuando la distribuyes des un punto central a todas las demás
+Particionada: está partida en cada una de las diversas zonas geográficas y se comparten información entre ellas.
+Replicada: tener la misma información en todas y entre ellas se hablan para siempre tener la misma versión.
+
+*/
+
+/* WINDOW FUNCTION */
+
+-- Obtener el número de tupla sin un orden en particular --
+SELECT ROW_NUMBER() OVER() AS row_id, *
+FROM tabla;
+
+-- Obtener el número de tupla cuando ordenamos por fecha de incorporacion --
+SELECT ROW_NUMBER() OVER(ORDER BY fecha_incorporacion) AS row_id, *
+FROM tabla;
+
+-- Obtener el valor de un atributo de la primera tupla del window frame actual (global por default) --
+SELECT FIRST_VALUE(colegiatura) OVER() AS row_id, *
+FROM tabla;
+
+-- Obtener el valor de un atributo de la primera tupla del window frame actual --
+-- La colegiatura de la persona que se inscribió primero por carrera --
+SELECT FIRST_VALUE(colegiatura) OVER(PARTITION BY carrera_id ORDER BY fecha_incorporacion) AS row_id, *
+FROM tabla;
+
+-- Obtener el valor de un atributo de la ultima tupla del window frame actual --
+-- La colegiatura de la persona que se inscribió al final por carrera --
+SELECT LAST_VALUE(colegiatura) OVER(PARTITION BY carrera_id ORDER BY fecha_incorporacion) AS row_id, *
+FROM tabla;
+
+-- Obtener el valor de un atributo de la tupla numero n = 3 del window frame actual --
+-- La colegiatura de la persona que se inscribió en lugar 3 por carrera --
+SELECT nth_value(colegiatura, 3) OVER(PARTITION BY carrera_id ORDER BY fecha_incorporacion) AS row_id, *
+FROM tabla;
+
+-- Rank Simple: Por cada elemento cuenta 1 generando espacios en el rank --
+SELECT 	*,
+		RANK() OVER (PARTITION BY carrera_id ORDER BY colegiatura DESC) AS brand_rank
+FROM	tabla
+ORDER BY carrera_id, brand_rank;
+
+-- Rank Simple: Por cada grupo de lementos iguales cuenta 1 generando rank "denso" --
+SELECT 	*,
+		DENSE_RANK() OVER (PARTITION BY carrera_id ORDER BY colegiatura DESC) AS brand_rank
+FROM	tabla
+ORDER BY carrera_id, brand_rank;
+
+-- Percent Rank: Genera una distribución percentual siguiendo la fórmula (rank - 1) / (total rows - 1) --
+SELECT 	*,
+		PERCENT_RANK() OVER (PARTITION BY carrera_id ORDER BY colegiatura ASC) AS brand_rank
+FROM	tabla
+ORDER BY carrera_id, brand_rank;
+
+
+-- ROW_NUMBER(): nos da el numero de la tupla que estamos utilizando en ese momento.
+
+-- OVER([PARTITION BY column] [ORDER BY column DIR]): nos deja Particionar y Ordenar la window function.
+
+-- PARTITION BY(column/s): es un group by para la window function, se coloca dentro de OVER.
+
+-- FIRST_VALUE(column): devuelve el primer valor de una serie de datos.
+
+-- LAST_VALUE(column): Devuelve el ultimo valor de una serie de datos.
+
+-- NTH_VALUE(column, row_number): Recibe la columna y el numero de row que queremos devolver de una serie de datos
+
+-- RANK(): nos dice el lugar que ocupa de acuerdo a el orden de cada tupla, deja gaps entre los valores.
+
+-- DENSE_RANK(): Es un rango mas denso que trata de eliminar los gaps que nos deja RANK.
+
+-- PERCENT_RANK(): Categoriza de acuerdo a lugar que ocupa igual que los anteriores pero por porcentajes.
+
 
